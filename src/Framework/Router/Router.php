dd2062c264221f1eb04e4f045b16b67af786c8f8
Steve <karyngtont@gmail.com>
2025-06-22 08:37:23 +0200

    router

--- src/Framework/Router/Router.php
+++ src/Framework/Router/Router.php
@@ -2,7 +2,114 @@
 
 namespace App\Framework\Router;
 
+use PhpDevCommunity\Exception\MethodNotAllowed;
+use PhpDevCommunity\Exception\RouteNotFound;
+use PhpDevCommunity\Route;
+use PhpDevCommunity\Router as PhpRouter;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\InvalidArgumentException;
+use function DI\add;
+
 class Router
 {
+
+    private $router;
+    public function __construct()
+    {
+        $this->router = new PhpRouter();
+    }
+
+
+    public function get(
+        string $name,
+        string $path,
+         $handler,
+        array $wheres=[]
+
+    ):void
+    {
+         $this->add($name,$path,$handler,['GET'],$wheres);
+    }
+
+    public function post(
+        string $name,
+        string $path,
+         $handler,
+        array $wheres=[]
+
+    ):void
+    {
+        $this->add($name,$path,$handler,['POST'],$wheres);
+    }
+
+    public function delete(
+        string $name,
+        string $path,
+         $handler,
+        array $wheres=[]
+
+    ):void
+    {
+        $this->add($name,$path,$handler,['DELETE'],$wheres);
+    }
+
+
+    public function crud($prefixName,$prefixPath,$handler){
+
+        $this->get("$prefixName.index",$prefixPath,[$handler,'index']);
+        $this->get("$prefixName.new","$prefixPath/new",[$handler,'create']);
+        $this->post("$prefixName.create","$prefixPath/new",[$handler,'create']);
+        $this->get("$prefixName.edit","$prefixPath/{id}",[$handler,'edit'],
+        ['id'=>'[0-9]+']);
+        $this->post("$prefixName.update","$prefixPath/{id}",[$handler,'edit'],
+        ['id'=>'[0-9]+']);
+        $this->delete("$prefixName.delete","$prefixPath/{id}",[$handler,'delete'],
+        ['id'=>'[0-9]+']);
+
+    }
+    public function match(ServerRequestInterface $serverRequest): ?Route
+    {
+        try {
+            return $this->router->match($serverRequest);
+        }catch (MethodNotAllowed|RouteNotFound ){
+            return  null;
+        }
+
+    }
+
+    public function generateUri(string $name, array $parameters = []): string
+    {
+        return $this->router->generateUri($name, $parameters);
+
+    }
+
+    public function has(string $name):bool{
+        try {
+            $this->generateUri($name);
+            return true;
+
+        }catch (InvalidArgumentException ){
+            return false;
+        }
+    }
+
+
+
+
+    private  function add(
+        string $name,
+        string $path,
+        string $handler,
+        array $methods ,
+        array $wheres=[]
+    ){
+
+        $route = new Route($path, $name, $handler, $methods);
+        foreach ($wheres as $key=>$regex) {
+            $route->where($key, $regex);
+        }
+        $this->router->add($route);
+
+    }
 
 }
